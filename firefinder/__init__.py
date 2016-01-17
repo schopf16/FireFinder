3d016e1a82853c81db6a469469c67b930ae15eaa
Michael Anderegg <m.anderegg@gmx.net>
2016-01-17 18:29:22 +0100

    [ADD] __init__.py --> new links added

--- firefinder/__init__.py
+++ firefinder/__init__.py
@@ -6,3 +6,8 @@ from firefinder.screenEvent     import ScreenEvent
 from firefinder.screenOff       import ScreenOff
 from firefinder.screenSlideshow import ScreenSlideshow
 from firefinder.cecLibrary      import tv_power
+
+# miscellaneous imports
+from firefinder.miscellaneous import createImage
+from firefinder.miscellaneous import getTextFontSize
+from firefinder.miscellaneous import RepeatingTimer
