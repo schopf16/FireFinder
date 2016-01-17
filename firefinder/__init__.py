dff71aea04d6c05e87eb4987940651786db4a2ca
Michael Anderegg <m.anderegg@gmx.net>
2016-01-17 10:07:28 +0100

    [UPD] __init__.py --> add firefinder classes

---
+++ firefinder/__init__.py
@@ -0,0 +1,8 @@
+"""Core package for FireFinder"""
+  
+from firefinder.sound           import alarmSound
+from firefinder.screenClock     import ScreenClock
+from firefinder.screenEvent     import ScreenEvent
+from firefinder.screenOff       import ScreenOff
+from firefinder.screenSlideshow import ScreenSlideshow
+from firefinder.cecLibrary      import tv_power
